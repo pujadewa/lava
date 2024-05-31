68fd1724ea2ba50095eeb3efbcc98ec036ab34da
puja dewa <pujdewa10@gmail.com>
2024-05-31 14:16:48 +0700

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-87d8d801-4d6f-481f-a6fb-834c0e2a9fce/"
 
 OUTPUT_DIR="logs"
 
